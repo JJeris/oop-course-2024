0ae116d007eee6ff8e58e825a04fd4fa42cd817a
jjeris <johans.justs.eris@gmail.com>
2024-04-21 14:18:06 +0300

    Finish Road.java

--- src/main/java/jtm/activity04/Road.java
+++ src/main/java/jtm/activity04/Road.java
@@ -5,17 +5,23 @@ public class Road {
 	private String to; // End point
 	private int distance; // distance in km
 
-	/*- TODO #1
+	/*- #1
 	 * Select menu Source — Generate Constructor using Fields...
 	 * and create constructor which sets from, to and distance
 	 * values of the newly created object
 	 */
-
+	public Road(String from, String to, int distance) {
+		this.from = from;
+		this.to = to;
+		this.distance = distance;
+	}
 	/*- TODO #2
 	 * Create constructor without parameters, which sets empty
 	 * values or 0 to all object properties
 	 */
-
+	public Road() {
+		this("", "", 0);
+	}
 
 	/*- TODO #3
 	 * Select menu: Source — Generate getters and Setters...
@@ -23,7 +29,29 @@ public class Road {
 	 * fields
 	 */
 
+	public String getFrom() {
+		return from;
+	}
 
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
 	/*- TODO #4
 	 * Select menu: Source — Generate toString()...
 	 * and implement this method, that it returns String in form:
@@ -34,5 +62,8 @@ public class Road {
 	 * See more at: https://en.wikipedia.org/wiki/Dash
 	 */
 
-
+	@Override
+	public String toString() {
+		return from + " — " + to + ", " + distance + "km";
+	}
 }
