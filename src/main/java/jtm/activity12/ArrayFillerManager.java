def8aa8e5c974498121768b15b346991cc1fd62d
jjeris <johans.justs.eris@gmail.com>
2024-06-02 14:06:20 +0300

    Finish activity 12

--- src/main/java/jtm/activity12/ArrayFillerManager.java
+++ src/main/java/jtm/activity12/ArrayFillerManager.java
@@ -29,8 +29,16 @@ public class ArrayFillerManager {
 		// initialize array with passed size
 		// initialize list of threads
 		// return reference to the initialized array
-		return null;
+//		return null;
+		ArrayFillerManager.array = new int[arraySize];
+		ArrayFillerManager.latency = latency;
+		ArrayFillerManager.startValue = startValue;
+		threads = new LinkedList<>();
+
+		return array;
 	}
+
+
 
 	public static void fillStupidly() {
 		// TODO create cycle which creates new ArrayFiller objects
@@ -40,6 +48,13 @@ public class ArrayFillerManager {
 		// current (main) thread.
 		// Note that this method emulates, what would happen if you would send
 		// just small portions of data through media with latency.
+
+		for (int i = 0; i < array.length - 1; i++) {
+			ArrayFiller arrayFiller = new ArrayFiller(latency, startValue, i, i);
+
+			arrayFiller.run();
+
+		}
 	}
 
 	public static void fillSequentially() {
@@ -48,6 +63,12 @@ public class ArrayFillerManager {
 		// Note that this method emulates, what would happen if you would do
 		// proper "buffering" with large amount of data, but do execution just
 		// in single thread.
+
+		for (int i = 0; i < array.length - 1; i++) {
+			ArrayFiller arrayFiller = new ArrayFiller(latency, startValue, 0, array.length - 1);
+
+			arrayFiller.run();
+		}
 	}
 
 	public static void fillParalelly() {
@@ -64,6 +85,25 @@ public class ArrayFillerManager {
 		// Note that this method emulates, what would happen if you do proper
 		// buffering and scaling of the execution.
 
+		int threadCount = 4;
+		int arraySize = array.length;
+		for (int i = 0; i < threadCount; i++) {
+			int from = i * arraySize / threadCount;
+			int to = (i+1) * arraySize / threadCount;
+
+			ArrayFiller arrayFiller = new ArrayFiller(latency, startValue, from, to);
+			Thread thread = new Thread(arrayFiller);
+			threads.add(thread);
+			thread.start();
+		}
+
+		for (Thread thread: threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+
+			}
+		}
 	}
 
 }
