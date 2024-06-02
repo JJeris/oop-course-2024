4308106a1c2a50411efd9c4b9ec461c0c2b335f8
jjeris <johans.justs.eris@gmail.com>
2024-06-02 14:43:53 +0300

    Finish activity 13

--- src/main/java/jtm/activity13/TeacherManager.java
+++ src/main/java/jtm/activity13/TeacherManager.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TeacherManager {
@@ -30,6 +31,14 @@ public class TeacherManager {
 		 * server-wise, not just database-wise.
 		 * 3. Set AutoCommit to false and use commit() where necessary in other methods
 		 */
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/?autoReconnect=true&serverTimezone=UTC&characterEncoding=utf8", user, password);
+			conn.setAutoCommit(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -48,6 +57,30 @@ public class TeacherManager {
 		 * 2. Do not't use transactions for search (autocommit=false, commit() is not called)
 		 *    because table should not be blocked for concurrent write during search
 		 */
+
+		String sql = "SELECT * FROM `database00`.`Teacher` where id=?;";
+		PreparedStatement preparedStatement;
+		ResultSet results;
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			results = preparedStatement.executeQuery();
+
+			if (results.first()) {
+				return new Teacher(
+						results.getInt("id"),
+						results.getString("firstname"),
+						results.getString("lastname")
+				);
+			}
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -68,7 +101,36 @@ public class TeacherManager {
 		 * Do not't use transactions for search (autocommit=false, commit() is not called)
 		 * because table should not be blocked for concurrent write during search
 		 */
-		return null;
+//		return null;
+
+		String sql = "SELECT * FROM `database00`.`Teacher` where firstname like ? and lastname like ?;";
+		PreparedStatement preparedStatement;
+		ResultSet results;
+		List<Teacher> teachers = new LinkedList<>();
+
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, "%" + firstName + "%");
+			preparedStatement.setString(2, "%" + lastName + "%");
+			results = preparedStatement.executeQuery();
+			while (results.first()) {
+				teachers.add(new Teacher(
+						results.getInt("id"),
+						results.getString("firstname"),
+						results.getString("lastname")
+				));
+			}
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+		return teachers;
+
 
 	}
 
@@ -82,8 +144,36 @@ public class TeacherManager {
 
 	public boolean insertTeacher(String firstName, String lastName) {
 		// TODO #4 Write an sql statement that inserts teacher in database.
+//		return false;
+
+		String sql = "INSERT INTO `database00`.`Teacher` (`firstname`, `lastname`) VALUES (?, ?);";
+		PreparedStatement preparedStatement;
+
+		int rowsAffected = 0;
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, firstName);
+			preparedStatement.setString(2, lastName);
+			rowsAffected = preparedStatement.executeUpdate();
+			conn.commit();
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+		if (rowsAffected > 0) {
+			return true;
+		}
+
 		return false;
+
 	}
+
+
 
 	/**
 	 * Insert teacher object into database
@@ -93,6 +183,32 @@ public class TeacherManager {
 	 */
 	public boolean insertTeacher(Teacher teacher) {
 		// TODO #5 Write an sql statement that inserts teacher in database.
+		String sql = "INSERT INTO `database00`.`Teacher` (`id`,`firstname`, `lastname`) VALUES (?, ?, ?);";
+		PreparedStatement preparedStatement;
+		int rowsAffected = 0;
+
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setInt(1, teacher.getId());
+			preparedStatement.setString(2, teacher.getFirstName());
+			preparedStatement.setString(3, teacher.getLastName());
+			rowsAffected = preparedStatement.executeUpdate();
+
+			conn.commit();
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+
+			e.printStackTrace();
+		}
+
+		if (rowsAffected > 0) {
+			return true;
+		}
+
 		return false;
 	}
 
@@ -105,6 +221,32 @@ public class TeacherManager {
 	 */
 	public boolean updateTeacher(Teacher teacher) {
 		// TODO #6 Write an sql statement that updates teacher information.
+		String sql = "UPDATE `database00`.`Teacher` SET `firstname` = ? `lastname` = ? WHERE `id` = ?;";
+		PreparedStatement preparedStatement;
+
+		int rowsAffected = 0;
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, teacher.getFirstName());
+			preparedStatement.setString(2, teacher.getLastName());
+			preparedStatement.setInt(3, teacher.getId());
+			rowsAffected = preparedStatement.executeUpdate();
+
+			conn.commit();
+		} catch (SQLException e) {
+
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+		if (rowsAffected > 0) {
+			return true;
+		}
+
 		return false;
 	}
 
@@ -117,11 +259,40 @@ public class TeacherManager {
 	 */
 	public boolean deleteTeacher(int id) {
 		// TODO #7 Write an sql statement that deletes teacher from database.
+		String sql = "DELETE FROM `database00`.`Teacher` WHERE `id` = ?;";
+		PreparedStatement preparedStatement;
+
+		int rowsAffected = 0;
+
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			rowsAffected = preparedStatement.executeUpdate();
+
+			conn.commit();
+		} catch (SQLException e) {
+
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+		if (rowsAffected > 0) {
+			return true;
+		}
 		return false;
 	}
 
 	public void closeConnecion() {
 		// TODO Close connection to the database server
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
